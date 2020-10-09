b99cf2b670c9e24fe149a0edda5876a403c3c8fe
AGGII <aggoins2@gmail.com>
2020-10-08 22:23:17 -0500

    Created eigth df

--- EmployeeSQL/Employee_db/Employee_db.sql
+++ EmployeeSQL/Employee_db/Employee_db.sql
@@ -120,3 +120,11 @@
 -- JOIN Departments ON Department_Employees.dept_no = Departments.dept_no
 -- WHERE Departments.dept_name = 'Sales'
 -- OR Departments.dept_name = 'Development';
+
+-- -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+-- SELECT last_name,
+-- COUNT (last_name) AS "frequency_count"
+-- FROM Employees
+-- GROUP BY last_name
+-- ORDER BY
+-- COUNT (last_name) DESC;
